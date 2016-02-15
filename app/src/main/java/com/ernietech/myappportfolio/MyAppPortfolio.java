541f408ac3fe456ce9bc2e1cd89ff4ff853d11a4
Ernie <ernie@ernietech.com>
2016-02-15 11:59:48 -0500

    Create basic UI in a fragment

--- app/src/main/java/com/ernietech/myappportfolio/MyAppPortfolio.java
+++ app/src/main/java/com/ernietech/myappportfolio/MyAppPortfolio.java
@@ -1,7 +1,13 @@
 package com.ernietech.myappportfolio;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MyAppPortfolio extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MyAppPortfolio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_app_portfolio);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ButtonViewFragment())
+                    .commit();
+        }
+    }
+
+    public static class ButtonViewFragment extends Fragment{
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//            return super.onCreateView(inflater, container, savedInstanceState);
+            //inflate fragment
+            return inflater.inflate(R.layout.fragment_app_launcher, container, false);
+        }
     }
 }
